8296ca208f7f19d91fcf0d0df0dba89b3555634a
amanda840604-sys <amanda840604@gmail.com>
2026-04-16 23:13:14 +0800

    feat: Update public assets and config
    Updates public assets with new images and modifies the vite base URL to './' for improved deployment flexibility.

--- test_keyshot.js
+++
@@ -1,5 +0,0 @@
-import http from 'http';
-
-http.get('http://localhost:3000/static/images/keyshot-logo-1.jpg', (res) => {
-  console.log('Keyshot Status:', res.statusCode);
-});
--- test_merry.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-
-http.get('http://localhost:3000/assets_img/images/merry_logo.jpg', (res) => {
-  console.log('Status:', res.statusCode);
-  console.log('Headers:', res.headers);
-}).on('error', (e) => {
-  console.error('Request error:', e.message);
-});
